bc087c79808d3b6e512721d629ae7a72e5a580d4
luis coco enriquez <luiscocoenriquez@hotmail.com>
2026-04-09 17:44:29 +0200

    fixing minor errors

--- src/SmartTaskManager.Web/Models/Forms/CreateUserFormModel.cs
+++ src/SmartTaskManager.Web/Models/Forms/CreateUserFormModel.cs
@@ -2,9 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SmartTaskManager.Web.Models.Forms;
 
-public sealed class CreateUserFormModel
+public sealed class CreateUserFormModel : IValidatableObject
 {
     [Required(AllowEmptyStrings = false)]
     [StringLength(100, MinimumLength = 1)]
     public string UserName { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrWhiteSpace(UserName))
+        {
+            yield return new ValidationResult(
+                "User name is required.",
+                new[] { nameof(UserName) });
+        }
+    }
 }
